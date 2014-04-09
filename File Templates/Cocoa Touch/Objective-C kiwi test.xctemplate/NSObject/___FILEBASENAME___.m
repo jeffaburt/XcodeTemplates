5b30f580101f034be0d323fb271c1670f1edf65e
Mark Schall <mark@detroitlabs.com>
2014-04-08 21:51:33 -0400

    Kiwi Template files

---
+++ File Templates/Cocoa Touch/Objective-C kiwi test.xctemplate/NSObject/___FILEBASENAME___.m
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Kiwi/Kiwi.h>
+#import "___VARIABLE_testClassName:identifier___.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+    describe(@"___VARIABLE_testClassName:identifier___", ^{
+		
+    });
+	
+SPEC_END
